e3c7e0a6712618f664f6f3a82f463d1c411d2a38
Brett Lyons <blyons@students.naropa.edu>
2015-12-17 17:01:06 -0700

    Trying some things

---
+++ fun-solution.js
@@ -0,0 +1,66 @@
+var solvable = [
+  [0, 0, 0, 0, 0, 0, 0, 1],
+  [0, 1, 0, 0, 0, 0, 0, 1],
+  [0, 1, 1, 1, 0, 0, 1, 1],
+  [0, 0, 0, 1, 0, 0, 1, 0],
+  [0, 0, 0, 1, 1, 1, 1, 0],
+  [0, 1, 1, 1, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0],
+];
+// you are given [7,1]
+
+var unsolvable = [
+  [0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 1, 1, 0, 1, 1, 1, 0],
+  [0, 1, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 1, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+];
+// given [0,3]
+
+
+function solve(graph, startingPoint) {
+
+  function checkAdjacents(previousCenter, currentCenter) {
+    // if we're at an end point, return true.
+    if(previousCenter !== currentCenter && (currentCenter[0] == graph.length - 1 || currentCenter[1] == graph.length[0] - 1)) { return true; }
+    potentialMoves = generatePotentialMove(currentCenter, previousCenter);
+    console.log(potentialMoves);
+
+      // console.log(potentialMoves.map(function (offsets) {
+      //   console.log("MAP HIT");
+      //   return checkAdjacents(currentCenter, graph[offsets[0] + currentCenter[0]][offsets[1] + currentCenter[1]]);
+      // }).some(function(element) {return element;}));
+    };
+
+  function generatePotentialMove(currentCenter, previousCenter) {
+    // generate a single array of spots to check.  Remove the 1 that matches previousCenter.
+    return [[1,0], [0,1], [-1,0], [0,-1]].filter(function (element) {
+      return ((element[0] + currentCenter[0] !== previousCenter[0]
+              && element[1] + currentCenter[1] !== previousCenter[1])
+        && (element[0] + currentCenter[0] >= 0
+            && element[1] + currentCenter[1] >= 0)
+              && (graph[element[0] + currentCenter[0]][element[1] + currentCenter[1]] === 1)); 
+    });
+  }
+  return checkAdjacents(startingPoint, [6,1]);
+}
+
+console.log(solve(solvable, [7,1])); // true
+console.log(solve(unsolvable, [0,3])); // false
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ snd-solution.js
@@ -0,0 +1,39 @@
+var solvable = [
+  [0, 0, 0, 0, 0, 0, 0, 1],
+  [0, 1, 0, 0, 0, 0, 0, 1],
+  [0, 1, 1, 1, 0, 0, 1, 1],
+  [0, 0, 0, 1, 0, 0, 1, 0],
+  [0, 0, 0, 1, 1, 1, 1, 0],
+  [0, 1, 1, 1, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 0, 0, 0, 0],
+];
+// you are given [7,1]
+
+var unsolvable = [
+  [0, 0, 0, 1, 0, 0, 0, 0],
+  [0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 1, 1, 0, 1, 1, 1, 0],
+  [0, 1, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0],
+  [0, 0, 0, 1, 1, 1, 1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0],
+];
+// given [0,3]
+
+
+function walkPath(graph, startingPoint) {
+  function moveOne(startingPoint, nextPoint) {
+    if(nextPoint[0] === graph[0].length && nextPoint[1] === graph.length) return true;
+    if(graph[nextPoint[0]][nextPoint[1]] === 1) {
+      return moveOne(nextPoint, [nextPoint[0] + 1, nextPoint[1] + 0]) && moveOne(nextPoint, [nextPoint[0] + 0, nextPoint[1] + 1]) && moveOne(nextPoint, [nextPoint[0] - 1, nextPoint[1] + 0]) && moveOne(nextPoint, [nextPoint[0] + 0, nextPoint[1] - 1]);
+    }
+    return false;
+  }
+  
+  return moveOne(startingPoint, [6,1]);
+}
+
+console.log(walkPath(solvable, [7,1]));
+console.log(walkPath(unsolvable, [0,3]));
